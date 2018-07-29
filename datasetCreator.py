3b68b670ed9ac3cc701d168ef20e4961a7724396
Avinash_Singh <desci.avinash@gmail.com>
2018-07-29 20:41:55 +0530

    Complete till training Dectection left

--- datasetCreator.py
+++ datasetCreator.py
@@ -10,12 +10,12 @@ id = input("Enter")
 sampleCount = 0
 while (True):
         ret, img = cam.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         faces = faceDetect.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
             sampleCount = sampleCount+1
             cv2.imwrite("dataSet/User."+str(id)+"."+ str(sampleCount)+ ".jpeg",gray[y:y+h,x:x+w])
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
             cv2.waitKey(100)
         cv2.imshow("Face", img)
         cv2.waitKey(1)
--- detector.py
+++ detector.py
@@ -8,7 +8,7 @@ rec = cv2.face.LBPHFaceRecognizer_create()
 rec.read("recognizer/trainingData.yml")
 
 id = 0
-font = cv2.InitFont(cv2.CV_FONT_HERSHEY_COMPLEX_SMALL,1,1,0,1)
+#font =cv2.CV_FONT_HERSHEY_SIMPLEX
 while(True):
         ret, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -16,7 +16,8 @@ while(True):
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             id,conf = rec.predict(gray[y:y+h,x:x+w])
-            cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,1,(255,255,255))
+            #cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,255)
+            cv2.putText(img, str(id), (x, y + h), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
         cv2.imshow("Face", img)
         if(cv2.waitKey(1)==ord('q')):
              break
